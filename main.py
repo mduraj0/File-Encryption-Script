2215ee2e71e09e73f02f15efae15f038b13465eb
Michał <durajczykmichal@gmail.com>
2022-11-17 14:08:22 +0100

    commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from argparse import ArgumentParser, Namespace
 from typing import Sequence
 
 
-
 class Password(argparse.Action):
     def __call__(self, parser, namespace, values, optional_string):
         if values is None:
@@ -18,30 +17,34 @@ def file_name(value: str):
     raise argparse.ArgumentError()
 
 
-parser = argparse.ArgumentParser(description='Decrypt encrypt app')
-parser.add_argument(
-    '-m',
-    '--mode',
-    choices=['encrypt', 'decrypt', 'append'],
-    required=True,
-    help='''encrypt --- encrypt file
-decrypt --- decrypt file
-append --- append text to encrypted file'''
-)
 
-parser.add_argument(
-    '-p',
-    '--password',
-    required=True,
-    nargs='?',
-    dest='password',
-    action=Password
-)
 
-parser.add_argument('-v', '--verbose', action='count', default=0)
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-f', '--file', action='append', type=file_name, help='list of files to process.')
-group.add_argument('-d', '--dir', help='path to folder with file to process')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Decrypt encrypt app')
+    parser.add_argument(
+        '-m',
+        '--mode',
+        choices=['encrypt', 'decrypt', 'append'],
+        required=True,
+        help='''encrypt --- encrypt file
+    decrypt --- decrypt file
+    append --- append text to encrypted file'''
+    )
 
-args = parser.parse_args()
-print(args)
+    parser.add_argument(
+        '-p',
+        '--password',
+        required=True,
+        nargs='?',
+        dest='password',
+        action=Password
+    )
+
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-f', '--file', action='append', type=file_name, help='list of files to process.')
+    group.add_argument('-d', '--dir', help='path to folder with file to process')
+
+    args = parser.parse_args()
+    print(args)
+
