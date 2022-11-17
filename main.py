9d136cec573723f6d8c18f9b27091a05552c962a
Michał <durajczykmichal@gmail.com>
2022-11-17 12:55:43 +0100

    commit

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def file_name(value: str):
     raise argparse.ArgumentError()
 
 
-
 parser = argparse.ArgumentParser(description='Decrypt encrypt app')
 parser.add_argument(
     '-m',
@@ -19,6 +18,11 @@ decrypt --- decrypt file
 append --- append text to encrypted file'''
 )
 
+parser.add_argument(
+    '-p'
+)
+
+parser.add_argument('-v', '--verbose', action='count', default=0)
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-f', '--file', action='append', type=file_name, help='list of files to process.')
 group.add_argument('-d', '--dir', help='path to folder with file to process')
