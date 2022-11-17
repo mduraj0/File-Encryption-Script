03d6d01fd924963924aeecdfb374e43558066471
Michał <durajczykmichal@gmail.com>
2022-11-17 12:39:02 +0100

    commit

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Decrypt encrypt app')
+parser.add_argument(
+    '-m',
+    '--mode',
+    choices=['encrypt', 'decrypt', 'append'],
+    required=True,
+    help='''encrypt --- encrypt file
+decrypt --- decrypt file
+append --- append text to encrypted file'''
+)
 
 args = parser.parse_args()
 print(args)
