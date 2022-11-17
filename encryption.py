fbf02971b01acadd923c7212811e78e35e5f0d0a
Michał <durajczykmichal@gmail.com>
2022-11-17 14:19:45 +0100

    commit

--- encryption.py
+++ encryption.py
@@ -3,6 +3,6 @@ class Encryption:
     def __init__(self, path):
         self.path = path
 
-
     def execute(self):
-
+        with open(self.path, 'r') as file:
+            data_to_encrypt = file.read()
