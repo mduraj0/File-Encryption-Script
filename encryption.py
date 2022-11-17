f75c95dce2d08d5b2d637d70327119123f163afb
Michał <durajczykmichal@gmail.com>
2022-11-17 14:20:39 +0100

    commit

--- encryption.py
+++ encryption.py
@@ -3,6 +3,12 @@ class Encryption:
     def __init__(self, path):
         self.path = path
 
+
+    def create_key(self, password):
+
+
     def execute(self):
         with open(self.path, 'r') as file:
             data_to_encrypt = file.read()
+
+        fernet = Fernet()
