f831a5fb00e6238a9cf8fff6e9fc6f4eddc19118
Michał <durajczykmichal@gmail.com>
2022-11-17 14:23:22 +0100

    commit

--- encryption.py
+++ encryption.py
@@ -1,11 +1,22 @@
 
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+
 class Encryption:
     def __init__(self, path):
         self.path = path
 
-
     def create_key(self, password):
+        salt = b'\xda\x01\xsa\asdasd\vdsd\x2131\sadbsa'
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=390000
+        )
 
+        key =
 
     def execute(self):
         with open(self.path, 'r') as file:
