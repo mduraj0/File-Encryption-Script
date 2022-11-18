ccef1efc415c6dfc301405a850d5a134023f38fd
Michał <durajczykmichal@gmail.com>
2022-11-18 13:41:04 +0100

    commit

--- encryption.py
+++ encryption.py
@@ -11,27 +11,4 @@ class EncryptDecrypt:
 
 
 class Encryption:
-    def __init__(self, path):
-        self.path = path
-
-    def create_key(self, password):
-        salt = b"\\xda\\x01\\xsa\\asd-asd\\dsd\\x2131\xadbsa"
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=390000
-        )
-
-        key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
-        return key
-
-    def execute(self, password):
-        with open(self.path, 'r') as file:
-            data_to_encrypt = file.read()
-
-        fernet = Fernet(self.create_key(password))
-        encrypted_content = fernet.encrypt(data_to_encrypt.encode('utf-8'))
-
-        with open(self.path.rename(self.path.with_suffix('.dokodu')), 'w') as file:
-            file.write(encrypted_content.decode('utf-8'))
+    pass
