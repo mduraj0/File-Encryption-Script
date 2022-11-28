0863931a82d50a2d9c1b390ad54955d2b84a31c0
Michał <durajczykmichal@gmail.com>
2022-11-28 10:38:27 +0100

    commit

--- encryption.py
+++ encryption.py
@@ -2,6 +2,7 @@ import base64
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
+from threading import Thread
 
 
 class EncryptDecrypt:
